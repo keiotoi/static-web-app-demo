52b3234c51cde729140557a6ea4bda2e2f1948b5
keiotoi0418 <keiotoi0418@gmail.com>
2020-06-22 13:01:03 +0900

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/NewSurfaceProducts.jpg">
+    <HelloWorld msg="Interested in Surface New Family?"/>
   </div>
 </template>
 
